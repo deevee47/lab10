9d1866667473499f1f381c75d017608d640520b2
deevee47 <divyanshvishwakarma4@gmail.com>
2025-05-09 16:59:34 +0530

    feat: added metdata

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -12,10 +12,8 @@ module.exports = {
   },
   networks: {
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/nA0KT5oVQ_EKD4kfLE0PCea0YER_p03-",
-      accounts: [
-        "cf2e0b108126f7bae50047b10df29721303dd865535fa161066c9a6fbddf637b",
-      ],
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
       gasPrice: 100000000,
     },
   },
